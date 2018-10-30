077f78f0571e35b1f99821c7d454941a90363ca8
Alen Nikolov <alen.nikolov@upnetix.com>
2018-10-30 08:09:28 +0200

    added more fields to save to mongo db

--- company-polls/server/routes/api.js
+++ company-polls/server/routes/api.js
@@ -4,8 +4,12 @@ const mongodb = require('../model/mongo');
 
 /* GET companies listing. */
 router.post('/', (req, res) => {
-  mongodb.insert({employeeName: req.body.employeeName, companyId: req.body.companyId,
-    experience: req.body.experience}).then((data) => {
+  const body = req.body;
+  mongodb.insert({
+    employeeName: body.employeeName, companyId: body.companyId,
+    experience: body.experience, lastPosition: body.lastPosition, salaryRange: body.salaryRange,
+    responsibilities: body.responsibilities
+  }).then((data) => {
     res.status(201).send('Your poll results were successfully saved!');
   });
 });
